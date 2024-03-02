c4135462269de4361f47c26651e037601b682989
Loucas Papalazarou <94012292+loucaspapalazarou@users.noreply.github.com>
2024-03-02 11:36:05 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,5 @@ sudo apt update
 sudo apt install docker.io -y
 sudo apt install docker-compose -y
 sudo usermod -aG docker $USER
-newgrp docker
 sudo apt install python3-pip
-
-docker-compose up -d -f ~/ttds-data-collection/docker-compose.yml
+sudo docker-compose up -d -f ~/ttds-data-collection/docker-compose.yml
