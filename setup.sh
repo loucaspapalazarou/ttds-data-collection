76545c382511722c47e7f9625db0c42320995268
Loucas Papalazarou <94012292+loucaspapalazarou@users.noreply.github.com>
2024-03-02 11:43:49 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,5 +6,10 @@ sudo apt install docker-compose -y
 sudo usermod -aG docker $USER
 sudo apt install python3-pip -y
 
+# Check if .env file exists, if not, copy .env.example to .env
+if [ ! -f .env ]; then
+    cp .env.example .env
+fi
+
 cd ~/ttds-data-collection
 sudo docker-compose up -d
